11bb1fb3bf3cdbf97a4d7acf6d3d21f7204c32a5
4pcbr <i4pcbr@gmail.com>
2013-03-03 01:48:45 +0400

    application created, looking for problem with empty certificate file

---
+++ application.rb
@@ -0,0 +1,88 @@
+require 'sinatra'
+require 'tempfile'
+require 'date'
+
+$opts = {
+  name: 'John Smith',
+  email: 'john@smith.com',
+  certificate: 'Company client certificate',
+  organization: 'Whitebox.io',
+  country: 'RU',
+  province: 'Moscow',
+  locality: 'Moscow'
+}
+
+$ssl = {
+  days: 180,
+  pass: 'y01_|s5Up3RseCr37CApW'
+}
+
+def clear_newlines( token )
+  token.gsub( /[\s\t\n\r\0\x0B]/, '' )
+end
+
+def format_spkac( key )
+  "SPKAC=#{clear_newlines( key )}" +
+  "\nCN=#{$opts[:name]}" +
+  "\nemailAddress=#{$opts[:email]}" +
+  "\n0.OU=#{$opts[:certificate]} client certificate" +
+  "\norganizationName=#{$opts[:organization]}" +
+  "\ncountryName=#{$opts[:country]}" +
+  "\nstateOrProvinceName=#{$opts[:province]}" +
+  "\nlocalityName=#{$opts[:locality]}"
+end
+
+def save_spkac!( spkac_key )
+  spkac_file = Tempfile.new(
+    ['cert', '.spkac'],
+    File.expand_path( './tmp/certificates' )
+  )
+  spkac_file.write( spkac_key )
+  system( "ls -la #{spkac_file.path}" )
+  spkac_file.close
+  spkac_file.path
+end
+
+def sign_spkac!( spkac_filename )
+  certificate_filename =  File.dirname( spkac_filename ) +
+                          File::SEPARATOR +
+                          File.basename( spkac_filename, '.spkac' )
+
+  sign_command =  "/usr/bin/env openssl " +
+                  "ca " + #-config #{ssl[:conf_file]} 
+                  "-days #{$ssl[:days]} " +
+                  "-notext -batch " +
+                  "-spkac #{spkac_filename} " +
+                  "-out #{certificate_filename} " +
+                  "-passin pass:'#{$ssl[:pass]}'"
+  p [ spkac_filename, certificate_filename ].to_s
+  certificate_filename
+end
+
+def respond_with_certificate( certificate )
+  headers = {
+    'Last-Modified' => Time.now.to_datetime.httpdate,
+    'Accept-Ranges' => 'bytes',
+    'Content-Length' => File.size( certificate ).to_s,
+    'Content-Type' => 'application/x-x509-user-cert'
+  }
+  headers.each_pair do |k, v|
+    response.headers[ k ] = v
+  end
+  File.read( certificate )
+end
+
+get '/' do
+  erb :index
+end
+
+post '/submit' do
+  key = params[ 'key' ]
+  if key.nil?
+    redirect '/'
+  end
+  spkac_key = format_spkac( key )
+  spkac_filename = save_spkac!( spkac_key )
+  signed_filename = sign_spkac!( spkac_filename )
+  respond_with_certificate( signed_filename )
+end
